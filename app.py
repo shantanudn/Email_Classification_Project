ca62554b5b2ee0f39c2bdd2d1fc5e140aef05548
shantanudn <shantanu.dn@gmail.com>
2020-05-19 23:14:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask,render_template,request
 import pickle
-from textblob import TextBlob
+import spacy
+nlp = spacy.load('en_core_web_sm')
+
 
 app = Flask(__name__)
 filename = 'svm_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
 cv=pickle.load(open('transform.pkl','rb'))
 
-def split_into_stems(message):
-    words = TextBlob(message).words
-    # for each word, take its "base form" = lemma 
-    return [word.stem() for word in words]
+def lemmatizer(text):        
+    sent = []
+    doc = nlp(text)
+    for word in doc:
+        sent.append(word.lemma_)
+    return " ".join(sent)
 
 @app.route('/')
 def home():
@@ -21,8 +25,7 @@ def predict():
 
     if request.method == 'POST':
         message = request.form['message']
-        message =split_into_stems(message)
-        message = ' '.join(message)
+        message = lemmatizer(message)
         data = [message]
         vect = cv.transform(data).toarray()
         my_prediction = clf.predict(vect)
@@ -31,4 +34,5 @@ def predict():
 
 if __name__ == '__main__':
 	app.run(debug=True)
-    
+
+
