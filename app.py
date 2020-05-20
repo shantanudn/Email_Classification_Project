62b022cd6b738b5326b7003af99eb50110e83348
shantanudn <shantanu.dn@gmail.com>
2020-05-20 17:07:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask,render_template,request
 import pickle
+import spacy
+nlp = spacy.load('en_core_web_sm')
+
 
 app = Flask(__name__)
 filename = 'svm_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
 cv=pickle.load(open('transform.pkl','rb'))
 
+def lemmatizer(text):        
+    sent = []
+    doc = nlp(text)
+    for word in doc:
+        sent.append(word.lemma_)
+    return " ".join(sent)
 
 @app.route('/')
 def home():
@@ -16,6 +25,7 @@ def predict():
 
     if request.method == 'POST':
         message = request.form['message']
+        message = lemmatizer(message)
         data = [message]
         vect = cv.transform(data).toarray()
         my_prediction = clf.predict(vect)
